84e7c3df9a0eb162fe234d8a2f76bef247bf8301
STMSCO NGO <stmsco.co@gmail.com>
2019-11-21 21:25:06 +0530

    'data'

--- btre/settings.py
+++ btre/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = 'ubsq#xai!qa_!dsi=61a&mbqn)lxtl*x48$fi0v-l^k%0z1^bn'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [127.0.0.1,'localhost']
+# ALLOWED_HOSTS = [127.0.0.1,'localhost']
 
-
+allowed_hosts=False
 # Application definition
 
 INSTALLED_APPS = [
